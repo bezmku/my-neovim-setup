44008502327818e84a6a3eeff09a672f7136c400
Beza <bezmku@gmail.com>
2026-04-21 20:46:16 +0300

    remvod test.java and test.class which werte accidentally pushed

--- test.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class test{
-    public static void main(String[] args) {
-    	ArrayList<String> list = new ArrayList<>();
-	list.add("name");	
-System.out.println(list);
-}
-}
