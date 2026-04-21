6af81c9eb9e91357751880bcc6473de96ae81e15
Beza <bezmku@gmail.com>
2026-04-21 22:42:10 +0300

    fixed the annoying telscope error message

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,27 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    'nvim-telescope/telescope.nvim',
+    -- Removed the tag to let it use the latest version with this workaround
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-	local builtin = require('telescope.builtin')
+        local telescope = require('telescope')
+        local builtin = require('telescope.builtin')
+
+        telescope.setup({
+            defaults = {
+                -- THE FIX: This stops Telescope from trying to 'color' the preview
+                -- which is what causes the 'ft_to_lang' crash in Nvim 0.12.0
+                preview = {
+                    treesitter = false,
+                },
+                file_ignore_patterns = { "target/.*", "%.class", ".git/.*" },
+                path_display = { "truncate" },
+            }
+        })
+
+        -- Your Keybindings
         vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-	vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-	vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     end
 }
