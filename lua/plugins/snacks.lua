9f624087a7b0b76b1d29f9c3db87ff2292e26bfa
Beza Mitiku <bezmku@gmail.com>
2026-06-08 17:42:52 +0300

    my own little change

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,10 +9,40 @@ return {
         statuscolumn = { enabled = true },
         words = { enabled = true },
         terminal = { enabled = true },
+        dashboard = {
+            enabled = true,
+            preset = {
+                header = [[
+       ██████╗██╗   ██╗██╗███╗   ███╗          Z
+      ██╔════╝██║   ██║██║████╗ ████║      Z
+      ██║     ██║   ██║██║██╔████╔██║   z
+      ██║     ╚██╗ ██╔╝██║██║╚██╔╝██║ z
+      ╚██████╗ ╚████╔╝ ██║██║ ╚═╝ ██║
+       ╚═════╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+]],
+                keys = {
+                    { icon = " ", key = "e", desc = "Open Neo-tree", action = "<cmd>Neotree toggle<cr>" },
+                    { icon = " ", key = "f", desc = "Find File", action = ":Telescope find_files" },
+                    { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                    { icon = " ", key = "g", desc = "Find Text", action = ":Telescope live_grep" },
+                    { icon = " ", key = "r", desc = "Recent Files", action = ":Telescope oldfiles" },
+                    { icon = " ", key = "c", desc = "Config", action = ":Telescope find_files cwd=" .. vim.fn.stdpath("config") },
+                    { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                },
+            },
+        },
     },
     keys = {
-        { "<leader>st", function() Snacks.terminal(nil, { win = { position = "float", border = "rounded", width = 0.8, height = 0.8, backdrop = 60, wo = { winblend = 10 } } }) end, desc = "Toggle Terminal" },
-        { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification History" },
-        { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+        {
+            "<leader>st",
+            function()
+                Snacks.terminal(nil,
+                    { win = { position = "float", border = "rounded", width = 0.8, height = 0.8, backdrop = 60, wo = { winblend = 10 } } })
+            end,
+            desc = "Toggle Terminal"
+        },
+        { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
+        { "<leader>un", function() Snacks.notifier.hide() end,         desc = "Dismiss All Notifications" },
     },
 }
